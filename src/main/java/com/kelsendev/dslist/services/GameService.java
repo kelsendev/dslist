416026f8b2d572aaeeb71eac68fc193cea701834
Kelsen Tavares <tiadsrussas@gmail.com>
2024-11-12 13:07:35 -0300

    Game repository, service, controller

---
+++ src/main/java/com/kelsendev/dslist/services/GameService.java
@@ -0,0 +1,23 @@
+package com.kelsendev.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kelsendev.dslist.dto.GameMinDTO;
+import com.kelsendev.dslist.entities.Game;
+import com.kelsendev.dslist.repositories.GameRepository;
+
+@Service
+public class GameService {
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	public List<GameMinDTO> findAll() {
+		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+
+}
