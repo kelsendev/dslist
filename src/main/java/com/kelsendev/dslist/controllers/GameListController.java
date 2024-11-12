33fccf3dcdab2bcbeea718f18ce65d89da119e0e
Kelsen Tavares <tiadsrussas@gmail.com>
2024-11-12 15:15:35 -0300

    GET lists

---
+++ src/main/java/com/kelsendev/dslist/controllers/GameListController.java
@@ -0,0 +1,26 @@
+package com.kelsendev.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kelsendev.dslist.dto.GameListDTO;
+import com.kelsendev.dslist.services.GameListService;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+	
+	@Autowired
+	private GameListService gameListService;
+	
+	@GetMapping
+	public List<GameListDTO> findAll() {
+		List<GameListDTO> result = gameListService.findAll();
+		return result;
+	}
+
+}
